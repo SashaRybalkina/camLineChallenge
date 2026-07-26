0b27430b432b5cc008c8cb049adecc2b223c2bfc
SashaRybalkina <sasharybalkina2003@gmail.com>
2026-07-26 14:30:28 -0600

    Nitpick, renamed LibraryTest to LibraryTests

--- src/test/java/com/camlineassignment/LibraryTests.java
+++ src/test/java/com/camlineassignment/LibraryTests.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class LibraryTest {
+public class LibraryTests {
 
     private LibraryRepository repo;
 
