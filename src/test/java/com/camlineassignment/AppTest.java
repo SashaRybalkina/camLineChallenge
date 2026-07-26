94132623b524f4092d4d6c3a592a2023b2fe4051
SashaRybalkina <sasharybalkina2003@gmail.com>
2026-07-26 13:05:54 -0600

    Initial commit

---
+++ src/test/java/com/camlineassignment/AppTest.java
@@ -0,0 +1,13 @@
+package com.camlineassignment;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
